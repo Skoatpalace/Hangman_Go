78c40602ea24094ad8538c602a9a55f0df16fe4f
Skoatpalace <richard.fages@gmail.com>
2020-03-07 07:43:56 +0100

    init struct & input

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"training.go/hangman/hangman"
+	"fmt"
+)
+
+
+func main() {
+
+	g := hangman.New(8, "Golang")
+	fmt.Println(g)
+
+	l, err := hangman.ReadGuess()
+	if err != nil {
+		fmt.Printf("Could not read from terminal: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println(l)
+}
