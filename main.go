3c07667450150b01d6df797e08008b87284dd1e3
Skoatpalace <richard.fages@gmail.com>
2020-03-07 09:12:30 +0100

    add State Game

--- main.go
+++ main.go
@@ -10,12 +10,26 @@ import (
 func main() {
 
 	g := hangman.New(8, "Golang")
-	fmt.Println(g)
+	
+	hangman.DrawWelcome()
 
-	l, err := hangman.ReadGuess()
-	if err != nil {
-		fmt.Printf("Could not read from terminal: %v", err)
-		os.Exit(1)
+	guess := ""
+
+	for {
+		hangman.Draw(g, guess)
+
+		switch g.State {
+		case "won","lost":
+			os.Exit(0)
+			
+		}
+		l, err := hangman.ReadGuess()
+		if err != nil {
+			fmt.Printf("Could not read from terminal: %v", err)
+			os.Exit(1)
+		}
+		guess = l
+		g.MakeAGuess(guess)
 	}
-	fmt.Println(l)
+
 }
